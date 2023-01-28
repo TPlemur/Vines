469721ec40d7e4c7963156d3db28a4e9bef348eb
Niccolo Abate <lofieloomis@gmail.com>
2023-01-28 13:28:32 -0800

    Added FMOD unity Integration and created simple test audio scene

---
+++ Assets/Scripts/Audio/CustomListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomListener : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player, cam;
+
+    [SerializeField]
+    private int listenerID;
+
+    FMOD.ATTRIBUTES_3D attributes = new FMOD.ATTRIBUTES_3D();
+
+    FMOD.VECTOR playerPosition;
+
+    void Update()
+    {
+        attributes.forward = FMODUnity.RuntimeUtils.ToFMODVector(cam.transform.forward);
+        attributes.up = FMODUnity.RuntimeUtils.ToFMODVector(cam.transform.up);
+        attributes.position = FMODUnity.RuntimeUtils.ToFMODVector(cam.transform.position);
+        playerPosition = FMODUnity.RuntimeUtils.ToFMODVector(player.transform.position);
+
+        FMODUnity.RuntimeManager.StudioSystem.setListenerAttributes(listenerID, attributes, playerPosition);
+    }
+}
