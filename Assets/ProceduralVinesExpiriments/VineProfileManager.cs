e415e46a6c8bf926530f7e9815ea69249d5d10e8
TPlemur <tplemur@gmail.com>
2023-03-16 14:43:21 -0700

    Made vine quality persistent across plays

--- Assets/ProceduralVinesExpiriments/VineProfileManager.cs
+++ Assets/ProceduralVinesExpiriments/VineProfileManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VineProfileManager : MonoBehaviour
 {
@@ -8,11 +9,17 @@ public class VineProfileManager : MonoBehaviour
     [SerializeField] ProceduralIvy MonIvy;
     MonVineStateMachine MVSM;
 
+    static int QualLevel = 2;
+    Slider slid;
+
 
     // Start is called before the first frame update
     void Start()
     {
         MVSM = GameObject.FindObjectOfType<MonVineStateMachine>();
+        slid = GetComponent<Slider>();
+        slid.value = QualLevel;
+        SetProfile((float)QualLevel);
     }
 
     public void SetProfile(float sliderVal)
@@ -20,5 +27,6 @@ public class VineProfileManager : MonoBehaviour
         int i = (int)sliderVal;
         MVSM.setSettings(MonProfiles[i]);
         MonIvy.setSettings(MonProfiles[i]);
+        QualLevel = i;
     }
 }
