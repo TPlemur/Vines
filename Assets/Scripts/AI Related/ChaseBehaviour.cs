7508fc6a27a08a8dafac3b1184eb9c92148965eb
rbm717 <47369758+rbm717@users.noreply.github.com>
2023-03-17 15:59:19 -0700

    Fixed chasing bugs

--- Assets/Scripts/AI Related/ChaseBehaviour.cs
+++ Assets/Scripts/AI Related/ChaseBehaviour.cs
@@ -11,26 +11,22 @@ public class ChaseBehaviour : StateMachineBehaviour
     Brain mobBrain;
     public float attackRange = 20;
     public float enemyspeed = 5;
-    float timer;
     float timeSpentCharging;
-    bool chargeCD = false;
-    bool charging = false;
 
     public float chargeSpeed;
     public float moveSpeed;
     Vector3 playerPos;
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       timer = 0;
-       Mob = animator.gameObject.GetComponentInParent<NavMeshAgent>();
-       //Mob.speed = enemyspeed;
-       Mob.speed = chargeSpeed;
-       mobBrain = Mob.GetComponentInChildren<Brain>();
-       timeSpentCharging = 0;
+        Mob = animator.gameObject.GetComponentInParent<NavMeshAgent>();
+        //Mob.speed = enemyspeed;
+        Mob.speed = chargeSpeed;
+        mobBrain = Mob.GetComponentInChildren<Brain>();
+        timeSpentCharging = 0;
         Debug.Log("IN CHASE STATE");
-       Player = GameObject.FindGameObjectWithTag("Player").transform;
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
 
         // AUDIO
         // set FMOD ChaseState to chasing
@@ -43,62 +39,27 @@ public class ChaseBehaviour : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        timeSpentCharging += Time.deltaTime;
-        float distance = Vector3.Distance(animator.transform.position, Player.position);
+        // Exits chase state if the player is in a hiding hole, otherwise charges them
         if(mobBrain.isHiding){
-                mobBrain.detectsPlayer = false;
-                animator.SetBool("isChasing", false);
-            }
-        //Debug.Log(playerPos);
-        if (distance < attackRange && !chargeCD && !charging){
+            mobBrain.detectsPlayer = false;
+            Mob.Stop();
+            Mob.ResetPath();
+            animator.SetBool("isChasing", false);
+        }else{
             Debug.Log("charging");
-            charging = true;
             playerPos = Player.position;
             Mob.SetDestination(playerPos);
             Mob.GetComponentInChildren<MonVineStateMachine>().currentState = MonVineStateMachine.state.charge;
-            //Mob.speed = chargeSpeed;
-        }else if (charging){
-            if (Vector3.Distance(animator.transform.position, playerPos) < 5){
-                //Mob.speed = moveSpeed;
-                charging = false;
-                chargeCD = true;
-                Debug.Log("finished charge");
-                //animator.SetBool("isChasing", false);
-            }
-            if(mobBrain.isHiding){
-                mobBrain.detectsPlayer = false;
-                animator.SetBool("isChasing", false);
-            }
-        }else{
-            Mob.SetDestination(Player.position);
         }
-        if (chargeCD){
-            timer += Time.deltaTime;
-        }
-        if (timer > 3){
-            Debug.Log("charge again");
-            chargeCD = false;
-            timer = 0;
-        }
+        // Stops chasing the player after a certain amount of time
         if(timeSpentCharging >= mobBrain.maxChaseTimer){
             mobBrain.detectsPlayer = false;
             animator.SetBool("isChasing", false);
         }
+        // Stops the monster from killing the player when the shield is deployed
         if(mobBrain.isShielded){
             animator.SetBool("isBlocked", true);
         }
-
-        // spaghetti audio code
-        /*
-        const float prob = 0.005f;
-        if (Random.value <= prob)
-        {
-            if (Random.value < 0.5f)
-                Mob.GetComponentInChildren<MonsterSounds>().Growl();
-            else
-                Mob.GetComponentInChildren<MonsterSounds>().RoarQuick();
-        }
-        */
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
