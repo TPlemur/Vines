540b82bdbc8be4b82456884d5fd2c93a6d65b59d
Niccolo Abate <lofieloomis@gmail.com>
2023-03-18 11:59:57 -0800

    Better (but unused currently) code for transitions in monster sfx

--- Assets/Scripts/Audio/MonsterSounds.cs
+++ Assets/Scripts/Audio/MonsterSounds.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Runtime.InteropServices;
+
+
 public class MonsterSounds : MonoBehaviour
 {
     public enum MONSTER_STATES { IDLE, CHASING, CLICK, SWALLOW, ROAR, GROWL, HOWL };
 
     public FMODUnity.StudioEventEmitter emitter = null;
+
+    FMOD.Studio.EVENT_CALLBACK markerCallback;
 
     // Start is called before the first frame update
     void Start()
@@ -15,13 +20,27 @@ public class MonsterSounds : MonoBehaviour
             emitter = GetComponent<FMODUnity.StudioEventEmitter>();
 
         emitter.Play();
+
+        markerCallback = new FMOD.Studio.EVENT_CALLBACK(MarkerEventCallback);
+        emitter.EventInstance.setCallback(MarkerEventCallback, FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_MARKER);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+    [AOT.MonoPInvokeCallback(typeof(FMOD.Studio.EVENT_CALLBACK))]
+    FMOD.RESULT MarkerEventCallback(FMOD.Studio.EVENT_CALLBACK_TYPE type, System.IntPtr _event, System.IntPtr parameterPtr)
+    {
+        var parameter = (FMOD.Studio.TIMELINE_MARKER_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_MARKER_PROPERTIES));
+        string marker = parameter.name;
+
+        Debug.Log(marker);
+
+        return FMOD.RESULT.OK;
+    }
+
 
     public void StartPreCharge()
     {
