58ec5912c1cdf1cd7f6c91313e3b51f402d64f59
jchen446 <jchen446@ucsc.edu>
2023-04-25 00:28:32 -0700

    ui texts

--- Assets/Scripts/UI Related/MainMenuScript.cs
+++ Assets/Scripts/UI Related/MainMenuScript.cs
@@ -2,13 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class MainMenuScript : MonoBehaviour
 {
+    public Text endingsDiscovered;
+    public bool firstEndingFound;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+    }
+
+    void Update()
+    {
+        if (firstEndingFound){
+             endingsDiscovered.GetComponent<UnityEngine.UI.Text>().text = "1/2";
+        }
     }
     //Call to get the player to start the elevator scene
     public void StartGame() 
@@ -25,6 +37,10 @@ public class MainMenuScript : MonoBehaviour
     public void BackToMainMenu() 
     {
         Time.timeScale = 1; //resume time if time was paused during last game
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "WinEndSceene"){
+            firstEndingFound = true;
+        }
         SceneManager.LoadScene(0);
     }
 
