9517b77c50aadf7b68d8890545608aa987fb118e
MatthewMeacham305 <121480047+MatthewMeacham305@users.noreply.github.com>
2023-04-30 17:54:25 -0700

    Fixed Endless Generation and Added PSX Setting
    Endless generation has been fixed by replacing a while loop with a foreachloop. A PSX setting was also added and can be changed from anywhere in the game.

--- Assets/Scripts/Procedural Generation/Warehouse.cs
+++ Assets/Scripts/Procedural Generation/Warehouse.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -26,7 +27,9 @@ public class Warehouse{
     public int rows;
 
     // List containing landmark rooms to make, add appropriate landmark subclass to list
-    private List<Landmark> special  = new List<Landmark>(new Landmark[] {new AlphaTeam(),  new Generator(), new ShieldRoom()});
+    private List<Landmark> alphaTeamRoom  = new List<Landmark>(new Landmark[] {new AlphaTeam()});
+    private List<Landmark> generatorRoom = new List<Landmark>(new Landmark[] { new Generator() });
+    private List<Landmark> shieldRoom = new List<Landmark>(new Landmark[] { new ShieldRoom() });
     private List<Landmark> cameras  = new List<Landmark>(new Landmark[] {new PVTMCamera(), new PVTMCamera(), new PVTMCamera(), new PVTMCamera()});
     private List<Landmark> hiding   = new List<Landmark>(new Landmark[] {new Hide(), new Hide(), new Hide(), new Hide()});
     private List<Landmark> tripwire = new List<Landmark>(new Landmark[] {new TripWire(), new TripWire(), new TripWire(), new TripWire()});
@@ -76,7 +79,9 @@ public class Warehouse{
      */
     public void Regenerate(){
         this.Teardown();
-        special  = new List<Landmark>(new Landmark[] {new AlphaTeam(),  new Generator(), new ShieldRoom()});
+        alphaTeamRoom  = new List<Landmark>(new Landmark[] {new AlphaTeam()});
+        generatorRoom  = new List<Landmark>(new Landmark[] {new Generator()});
+        shieldRoom  = new List<Landmark>(new Landmark[] {new ShieldRoom()});
         cameras  = new List<Landmark>(new Landmark[] {new PVTMCamera(), new PVTMCamera(), new PVTMCamera(), new PVTMCamera()});
         hiding   = new List<Landmark>(new Landmark[] {new Hide(), new Hide(), new Hide(), new Hide()});
         tripwire = new List<Landmark>(new Landmark[] {new TripWire(), new TripWire(), new TripWire(), new TripWire()});
@@ -200,7 +205,9 @@ public class Warehouse{
 
     // Place All Landmark rooms
     private void PlaceLandmarks(){
-        this.PlaceLandmarksFrom(this.special);
+        this.PlaceLandmarksFrom(this.alphaTeamRoom);
+        this.PlaceLandmarksFrom(this.generatorRoom);
+        this.PlaceLandmarksFrom(this.shieldRoom);
         this.PlaceLandmarksFrom(this.cameras);
         this.PlaceLandmarksFrom(this.hiding);
         this.PlaceLandmarksFrom(this.tripwire);
@@ -215,17 +222,14 @@ public class Warehouse{
      * Landmarks to place.
      */
     private void PlaceLandmarksFrom(List<Landmark> list){
-        while(list.Count != 0){
-            Room random = GetRandomGeneric();
-            foreach(Landmark landmark in list){
-                float dist = random.DistanceTo(this.startRoom);
-                if(landmark.InRange(dist)){
-                    random.type = landmark;
-                    random.ConnectToRandom();
-                    list.RemoveAt(list.IndexOf(landmark));
-                    break;
-                }
-            }
+        List<Room> roomList = GetGenericInRange(list[0].minDist, list[0].maxDist);
+        foreach (Landmark landmark in list)
+        {
+            if (roomList.Count == 0) break;
+            Room random = roomList[UnityEngine.Random.Range(0, roomList.Count - 1)];
+            random.type = landmark;
+            random.ConnectToRandom();
+            roomList.RemoveAt(roomList.IndexOf(random));
         }
     }
 
@@ -238,16 +242,21 @@ public class Warehouse{
         return data[row][column];
     }
 
-    /* GetRandomGeneric() will choose a random genric room
+    /* GetGenericInRange() will generate a list of genric rooms within the given range
      */
-    private Room GetRandomGeneric(){
-        while(true){
-            int row = UnityEngine.Random.Range(0, this.rows - 1);
-            int column = UnityEngine.Random.Range(0, this.columns -1);
-            if(data[row][column].type.GetType() == typeof(Generic)){
-                return data[row][column];
+    private List<Room> GetGenericInRange(float min, float max)
+    {
+        List<Room> list = new List<Room>();
+        foreach (List<Room> row in data){
+            foreach (Room room in row){
+                float dist = room.DistanceTo(this.startRoom);
+                if (dist >= min && dist <= max && room.type.GetType() == typeof(Generic))
+                {
+                    list.Add(room);
+                }
             }
         }
+        return list;
     }
 
     /* PlacePlayerAndMonster() will place the player, camera, 
