5a3efe24a603a763e96c40d6a4058eefae4f35b0
TPlemur <tplemur@gmail.com>
2023-06-08 15:17:47 -0700

    Added assets credit page, tweaked main layout

---
+++ Assets/Scripts/UI Related/HyperLinks.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HyperLinks : MonoBehaviour
+{
+    public void OpenFence()
+    {
+        Application.OpenURL("https://assetstore.unity.com/packages/3d/chainlink-fences-73107");
+    }
+
+    public void OpenTank()
+    {
+        Application.OpenURL("https://assetstore.unity.com/packages/3d/props/industrial/industrial-tanks-package-122228 ");
+    }
+
+    public void OpenShip()
+    {
+        Application.OpenURL("https://assetstore.unity.com/packages/3d/environments/shippingcontainer-147902 ");
+    }
+}
